51208b2df5168c82048b09343894a4bf532ac6a9
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-28 15:38:07 -0500

    mongo testing works

---
+++ test/notes.test.js
@@ -0,0 +1,195 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const app = require('../server');
+const { TEST_MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+const { notes } = require('../db/seed/notes');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Noteful API resource', function (){
+
+  // we need each of these hook functions to return a promise
+  // otherwise we'd need to call a `done` callback. `runServer`,
+  // `seedRestaurantData` and `tearDownDb` each return a promise,
+  // so we return the value returned by these function calls.
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function () {
+    return Note.insertMany(notes);
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+  // describe('GET endpoint', () => {
+  //   it('should return all existing notes', () => {
+
+  //     let note;
+  //     return chai.return()
+  //   } 
+  // });
+
+
+  describe('POST /api/notes', function () {
+    it('should create and return a new item when provided valid data', function () {
+      const newItem = {
+        'title': 'The best article about cats ever!',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...'
+      };
+
+      let res;
+      // 1) First, call the API
+      return chai.request(app)
+        .post('/api/notes')
+        .send(newItem)
+        .then(function (_res) {
+          res = _res;
+          expect(res).to.have.status(201);
+          expect(res).to.have.header('location');
+          // console.log(res);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+          // 2) then call the database
+          return Note.findById(res.body.id);
+        })
+        // 3) then compare the API response to the database results
+        .then(data => {
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.title).to.equal(data.title);
+          expect(res.body.content).to.equal(data.content);
+          expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+          expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+        });
+    });
+  });
+
+  describe('GET /api/notes/:id', function () {
+    it('should return correct note', function () {
+      let data;
+      // 1) First, call the database
+      return Note.findOne()
+        .then(_data => {
+          data = _data;
+          // 2) then call the API with the ID
+          return chai.request(app).get(`/api/notes/${data.id}`);
+        })
+        .then((res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+
+          // 3) then compare database results to API response
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.title).to.equal(data.title);
+          expect(res.body.content).to.equal(data.content);
+          expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+          expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+        });
+    });
+  });
+
+
+  describe('GET /api/notes', function () {
+
+    it('should return all notes', () => {
+      // 1) Call the database **and** the API
+    // 2) Wait for both promises to resolve using `Promise.all`
+      return Promise.all([
+        Note.find(),
+        chai.request(app).get('/api/notes')
+      ])
+      // 3) then compare database results to API response
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+        });
+    });
+
+  });
+   
+
+  describe('PUT /api/notes', function () {
+    it('should update fields you send over', function () {
+      const updateData = {
+        'id': '000000000000000000000000',
+        'title': 'updated title',
+        'content': 'updated content.'
+      };
+
+      let res;
+      // 1) First, call the API
+      return Note
+        .findOne()
+        .then(function(_note) {
+          updateData.id = _note.id;
+
+          // make request then inspect it to make sure it reflects
+          // data we sent
+          return chai.request(app)
+            .put(`/api/notes/${_note.id}`)
+            .send(updateData);
+        })
+        .then(function (_res) {
+          res = _res;
+          expect(res).to.have.status(200);
+          // console.log(res);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+          // 2) then call the database
+          return Note.findById(res.body.id);
+        })
+        // 3) then compare the API response to the database results
+        .then(data => {
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.title).to.equal(data.title);
+          expect(res.body.content).to.equal(data.content);
+        });
+    });
+  });
+
+  describe('DELETE /api/notes', () => {
+
+    it('should delete note by id', () => {
+
+      let note;
+
+      return Note
+        .findOne()
+        .then((_note) => {
+          note = _note;
+          return chai.request(app).delete(`/api/notes/${note.id}`);
+        })
+        .then((res) => {
+          expect(res).to.have.status(204);
+          return Note.findById(note.id);
+        })
+        .then((_note) => {
+          expect(_note).to.be.null;
+        });
+    });
+  });
+
+
+});
