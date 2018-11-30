df1b8954178ef83b4eb109a71e9bb88b110d9584
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-30 13:04:39 -0500

    all tag routes working postman

---
+++ models/tag.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const tagsSchema = new mongoose.Schema({
+  name : {
+    type: String, 
+    required: true,
+    unique: true
+  }
+});
+
+tagsSchema.set('timestamps', true);
+
+tagsSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Tags', tagsSchema);
